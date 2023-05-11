2dd5af5cf225d729c2a14b7efc6d3baefc98be8f
Vinod-Jantikar <vinodjantikar999@gmail.com>
2023-05-11 17:30:23 +0530

    declaration of constants

---
+++ Day1/constants.php
@@ -0,0 +1,14 @@
+<?php
+
+// Constants can be declared by 2 ways 
+// 1. By define function 
+// define('variable name', 'value')
+
+define('STATUS_PAID', 'PAID');
+echo STATUS_PAID;
+
+// 2. By const keyword 
+
+const NAME = "JOHN_DOE";
+print NAME;
+
